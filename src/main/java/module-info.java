fac5de205a5279193791d65d5cedc476ac2a70e2
alonsonunez <alonsonunez.b94@gmail.com>
2026-02-26 12:19:03 -0600

    clean state

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.compassfx.demo {
-    requires javafx.graphics;
-    requires javafx.controls;
-
-    opens com.compassfx.controls to javafx.graphics, javafx.fxml;
-
-    exports com.compassfx.demo;
-    exports com.compassfx.controls;
-}
